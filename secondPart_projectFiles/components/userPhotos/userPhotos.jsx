abee373d01b741dd52b5aa3367c9fa8b30a1e096
M <marlena.berels@student.uni-siegen.de>
2021-10-29 17:01:37 +0200

    useless commit

--- secondPart_projectFiles/components/userPhotos/userPhotos.jsx
+++ secondPart_projectFiles/components/userPhotos/userPhotos.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import {
-  Box,
-  Typography
-} from '@material-ui/core';
+/*import {
+  Box
+  /*ImageList,
+  ImageListItem,
+  ImageListItemBar,
+  ImageListItemBar,
+  ListItem
+} from '@mui/material';*/
+import Box from '@mui/material/Box'
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
 import './userPhotos.css';
 
 
@@ -12,12 +22,15 @@ import './userPhotos.css';
 class UserPhotos extends React.Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      photos : window.cs142models.photoOfUserModel(this.props.match.params.userId)
+    }
+    console.log(this.state.photos)
   }
 
   render() {
     return (
-      <Typography variant="body1">
+      /*<Typography variant="body1">
       This should be the UserPhotos view of the PhotoShare app. Since
       it is invoked from React Router the params from the route will be
       in property match. So this should show details of user:
@@ -26,8 +39,41 @@ class UserPhotos extends React.Component {
         <Typography variant="caption">
           {JSON.stringify(window.cs142models.photoOfUserModel(this.props.match.params.userId))}
         </Typography>
-      </Typography>
+      </Typography>*/
 
+      /*_id  (string) - The ID of the photo
+      *   date_time (date) - he date and time the picture was taken in ISO format.
+      *   file_name (string) - The file name in the image directory of the picture.
+      *   user_id (string) - The user id of the picture's owner.
+      *   comments: {array of objects} - An array of comment objects containing the properties:
+      *        _id  (string) - The ID of the comment.
+      *        date_time (date) - The date the comment was made in ISO format.
+      *        comment (string) - The text of the comment.
+      *        user: {object} The user info (see userMode for format) who made the comment
+      *        photo_id: (string) - The ID of the photo the comment belongs to.*/
+
+      <Box sx={{ width: 500, height: 450, overflowY: 'scroll' }}>
+        <ImageList variant="masonry" cols={3} gap={8}>
+          {this.state.photos.map((item) => (
+            <ImageListItem key={item._id}>
+              <ImageListItemBar position="above" title={item.date_time} />
+              <img
+                src={`${item.File_name}`}
+                alt={item.File_name}
+                loading="lazy"
+              />
+              <h2>Comments</h2>
+              <List>
+              {/*{item.comments.map((comment) => (
+                <ListItem disablePadding>
+                  Comment
+                </ListItem>
+              ))}*/}
+              </List>
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </Box>
     );
   }
 }
