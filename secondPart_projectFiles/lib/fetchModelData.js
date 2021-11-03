6f6bd4b0b932d9282f2d523fa9cf2c1ab49c2650
Martin Araya <martin.araya.zavala@gmail.com>
2021-11-03 12:56:33 +0100

    api implemented

--- secondPart_projectFiles/lib/fetchModelData.js
+++ secondPart_projectFiles/lib/fetchModelData.js
@@ -16,11 +16,18 @@ var Promise = require("Promise");
 
 
 function fetchModel(url) {
-  return new Promise(function(resolve, reject) {
-      console.log(url);
-      setTimeout(() => reject({status: 501, statusText: "Not Implemented"}),0);
-      // On Success return:
-      // resolve({data: getResponseObject});
+  return new Promise(function (resolve, reject) {
+    let request = new XMLHttpRequest();
+    request.open('GET', "http://localhost:3000/" + url);
+    request.responseType = "json";
+    request.onload = () => {
+      resolve(request.response);
+    }
+    request.send();
+    //console.log(url);
+    //setTimeout(() => reject({status: 501, statusText: "Not Implemented"}),0);
+    // On Success return:
+    // resolve({data: getResponseObject});
   });
 }
 
