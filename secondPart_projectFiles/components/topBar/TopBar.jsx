dcbdc2ab0e0a7cbdeeb44037f2c9a05ac0fc84c4
Martin Araya <martin.araya.zavala@gmail.com>
2021-11-20 18:58:17 +0100

    changed orientation of text in TopBar

--- secondPart_projectFiles/components/topBar/TopBar.jsx
+++ secondPart_projectFiles/components/topBar/TopBar.jsx
@@ -12,38 +12,38 @@ class TopBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      user_id : "", // name of user
-      group_name : "group 47", // name of group
-      url : window.location.href, // current url
-      home_page : false, // flag if user is on homepage
-      photos : false, // flag if user is on on photopage
-      v : "" // v value displaying on homepage
+      user_id: "", // name of user
+      group_name: "group 47", // name of group
+      url: window.location.href, // current url
+      home_page: false, // flag if user is on homepage
+      photos: false, // flag if user is on on photopage
+      v: "" // v value displaying on homepage
     }
     window.addEventListener("popstate", () => { // run this code everytime url changes
       this.marlenas_func();
     })
   }
 
-  marlenas_func(){
-    if(window.location.href.split("/").length > 5){ //check if it is on homepage or not
+  marlenas_func() {
+    if (window.location.href.split("/").length > 5) { //check if it is on homepage or not
       fetchModel("user/" + window.location.href.split("/")[5]).then((value) => { // get user name from url
-        this.setState({ url : window.location.href, user_id : value.first_name, home_page : false, photos : false});
-        if(window.location.href.split("/")[4] == "photos"){ // if user clicked on photos chage the flag
-          this.setState({ photos : true});
+        this.setState({ url: window.location.href, user_id: value.first_name, home_page: false, photos: false });
+        if (window.location.href.split("/")[4] == "photos") { // if user clicked on photos chage the flag
+          this.setState({ photos: true });
         }
       })
-    }else{ // if it is on homepage update v value
+    } else { // if it is on homepage update v value
       fetchModel("test/info").then((value) => { // get v value
-        this.setState({ url : window.location.href, home_page : true, v : value.__v, photos : false, user_id : ""})
+        this.setState({ url: window.location.href, home_page: true, v: value.__v, photos: false, user_id: "" })
       })
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.marlenas_func()
   }
 
-  componentDidUpdate(){
+  componentDidUpdate() {
   }
 
 
@@ -53,14 +53,14 @@ class TopBar extends React.Component {
     return (
       <AppBar className="cs142-topbar-appBar" position="absolute">
         <Toolbar display="flex">
-          <Typography style={{width:"80%"}} align="left" variant="h5" color="inherit">
-              {this.state.photos ? "Photos of " + this.state.user_id : this.state.user_id}
+          <Typography style={{ width: "20%" }} align="left" variant="h5" color="inherit">
+            {this.state.group_name}
+            {this.state.home_page ? " v: " + this.state.v : ""}
           </Typography>
-          <Typography style={{width:"20%"}} align="right" variant="h5" color="inherit">
-              {this.state.group_name}
-              {this.state.home_page? " v: " + this.state.v : ""}
+          <Typography style={{ width: "80%" }} align="right" variant="h5" color="inherit">
+            {this.state.photos ? "Photos of " + this.state.user_id : this.state.user_id}
           </Typography>
-          </Toolbar>
+        </Toolbar>
       </AppBar>
     );
   }
